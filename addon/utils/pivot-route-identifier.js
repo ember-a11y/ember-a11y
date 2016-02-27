58072ad086b4aa99167d7efa3088ba4084859f90
Nathan Hammond <github.com@nathanhammond.com>
2016-02-27 15:10:19 -0800

    Implement all expected scenarios

--- addon/utils/pivot-route-identifier.js
+++ addon/utils/pivot-route-identifier.js
@@ -1,8 +1,10 @@
 export default function pivotRouteIdentifier(oldHandlers, newHandlers) {
-
   if (oldHandlers === undefined) {
     return newHandlers[0];
   }
+
+  console.log(oldHandlers.map(handler => handler.handler.routeName));
+  console.log(newHandlers.map(handler => handler.handler.routeName));
 
   let length = Math.min(oldHandlers.length, newHandlers.length);
   let offset = oldHandlers.length >= newHandlers.length ? 1 : 0;
@@ -19,8 +21,10 @@ export default function pivotRouteIdentifier(oldHandlers, newHandlers) {
 
     if (oldHandler.handler.routeName === newHandler.handler.routeName) {
       commonHandlers.push(oldHandler);
+    } else if (commonHandlers.length) {
+      return newHandlers[i];
     } else {
-      return commonHandlers[commonHandlers.length-1];
+      return newHandlers[0];
     }
   }
   return newHandlers[length - offset];
--- tests/unit/utils/pivot-route-identifier-test.js
+++ tests/unit/utils/pivot-route-identifier-test.js
@@ -11,41 +11,11 @@ let mockInfoHandler = function(routeName) {
   };
 };
 
-test('identifies the last common node', function(assert) {
+test('navigating to another subroute', function(assert) {
   let oldHandlers = [
     mockInfoHandler('photos'),
     mockInfoHandler('photos.caption'),
     mockInfoHandler('photos.caption.new')
-  ];
-  let newHandlers = [
-    mockInfoHandler('photos'),
-    mockInfoHandler('photos.caption'),
-    mockInfoHandler('photos.caption.edit')
-  ];
-
-  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
-  assert.equal(infoHandler.handler.routeName, 'photos.caption');
-});
-
-test('can deal with more old handlers than new handlers', function(assert) {
-  let oldHandlers = [
-    mockInfoHandler('photos'),
-    mockInfoHandler('photos.caption'),
-    mockInfoHandler('photos.caption.new')
-  ];
-  let newHandlers = [
-    mockInfoHandler('photos'),
-    mockInfoHandler('photos.caption'),
-  ];
-
-  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
-  assert.equal(infoHandler.handler.routeName, 'photos.caption');
-});
-
-test('can deal with more new handlers than old handlers', function(assert) {
-  let oldHandlers = [
-    mockInfoHandler('photos'),
-    mockInfoHandler('photos.caption'),
   ];
   let newHandlers = [
     mockInfoHandler('photos'),
@@ -82,4 +52,61 @@ test('can deal with the no handlers', function(assert) {
   assert.throws(function() {
     pivotRouteIdentifier([], []);
   });
+});
+
+test('navigating to a subroute', function(assert) {
+  let oldHandlers = [
+    mockInfoHandler('photos'),
+  ];
+
+  let newHandlers = [
+    mockInfoHandler('photos'),
+    mockInfoHandler('photos.caption'),
+  ];
+
+  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
+  assert.equal(infoHandler.handler.routeName, 'photos.caption');
+});
+
+test('navigating to a parent route', function(assert) {
+  let oldHandlers = [
+    mockInfoHandler('photos'),
+    mockInfoHandler('photos.caption'),
+  ];
+
+  let newHandlers = [
+    mockInfoHandler('photos'),
+  ];
+
+  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
+  assert.equal(infoHandler.handler.routeName, 'photos');
+});
+
+test('navigating two steps back and one step forward', function(assert) {
+  let oldHandlers = [
+    mockInfoHandler('photos'),
+    mockInfoHandler('photos.caption'),
+    mockInfoHandler('photos.caption.edit'),
+  ];
+
+  let newHandlers = [
+    mockInfoHandler('photos.surprise'),
+  ];
+
+  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
+  assert.equal(infoHandler.handler.routeName, 'photos.surprise');
+});
+
+test('index to other route', function(assert) {
+  let oldHandlers = [
+    mockInfoHandler('index'),
+  ];
+
+  let newHandlers = [
+    mockInfoHandler('photos'),
+    mockInfoHandler('photos.caption'),
+  ];
+
+  let infoHandler = pivotRouteIdentifier(oldHandlers, newHandlers);
+  assert.equal(infoHandler.handler.routeName, 'photos');
 });
