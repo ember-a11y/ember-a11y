7c8b8bc7772db9cb0ec95765b1313a4fbbd0ec9c
Nathan Hammond <github.com@nathanhammond.com>
2016-02-27 16:22:04 -0800

    Refactor the linking approach.

--- app/instance-initializers/ember-a11y.js
+++ app/instance-initializers/ember-a11y.js
@@ -2,16 +2,23 @@
 // imports happen before the app boots.
 import Ember from 'ember';
 import { registerKeywords } from 'ember-a11y/ember-internals';
+import pivotRouteIdentifier from 'ember-a11y/utils/pivot-route-identifier';
+
 registerKeywords();
 
-let handlerInfos;
+let stashedHandlerInfos = {};
+
 Ember.Router.reopen({
   didTransition(handlerInfos) {
+    let pivotHandler = pivotRouteIdentifier(stashedHandlerInfos.handlerInfos, handlerInfos);
+    stashedHandlerInfos.handlerInfos = handlerInfos;
     this._super(...arguments);
   }
 });
 
-export function initialize(application) {}
+export function initialize(application) {
+  application._stashedHandlerInfos = stashedHandlerInfos;
+}
 
 export default {
   name: 'ember-a11y',
