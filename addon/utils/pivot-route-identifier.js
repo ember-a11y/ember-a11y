6bc4ddf1838adbfc2ca93a3f4fb8d665461154f8
Nathan Hammond <github.com@nathanhammond.com>
2016-02-27 16:21:14 -0800

    Refactor pivot route.

--- addon/utils/pivot-route-identifier.js
+++ addon/utils/pivot-route-identifier.js
@@ -3,9 +3,6 @@ export default function pivotRouteIdentifier(oldHandlers, newHandlers) {
     return newHandlers[0];
   }
 
-  console.log(oldHandlers.map(handler => handler.handler.routeName));
-  console.log(newHandlers.map(handler => handler.handler.routeName));
-
   let length = Math.min(oldHandlers.length, newHandlers.length);
   let offset = oldHandlers.length >= newHandlers.length ? 1 : 0;
 
@@ -13,18 +10,12 @@ export default function pivotRouteIdentifier(oldHandlers, newHandlers) {
     throw new Error('No handlers detected.');
   }
 
-  let commonHandlers = [];
   for (let i = 0; i < length; i++) {
-
     let oldHandler = oldHandlers[i];
     let newHandler = newHandlers[i];
 
-    if (oldHandler.handler.routeName === newHandler.handler.routeName) {
-      commonHandlers.push(oldHandler);
-    } else if (commonHandlers.length) {
+    if (oldHandler.handler.routeName !== newHandler.handler.routeName) {
       return newHandlers[i];
-    } else {
-      return newHandlers[0];
     }
   }
   return newHandlers[length - offset];
