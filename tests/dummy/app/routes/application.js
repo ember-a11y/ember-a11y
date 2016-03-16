aae1175995741a9e55bb4cc7ca5b2fd4c380a45e
Nathan Hammond <github.com@nathanhammond.com>
2016-03-15 19:25:00 -0700

    Make it possible to run this locally and on GitHub Pages.

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
+  model() {
+    let prefix = config.locationType === 'hash' ? '#/' : './';
+    return { prefix };
+  },
   actions: {
     reload() {
       window.location.reload(true);
