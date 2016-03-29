80cbfd430e9bafd12227396aab5fa1c41db42304
Dig <dig.vargas@locusenergy.com>
2016-03-28 22:07:19 -0700

    Moves helper to demo app, not the addon itself. Updates relevant test.

--- app/helpers/github-issue.js
+++ app/helpers/github-issue.js
@@ -1,1 +1,16 @@
-export { default, githubIssue } from 'ember-a11y/helpers/github-issue';
+import Ember from 'ember';
+
+export function githubIssue([title]) {
+	let titleStr = encodeURIComponent(`Demo App Test fail: ${title}`);
+	let linkEl = document.createElement('a');
+	let pEl = document.createElement('p');
+	linkEl.href = 
+		`https://github.com/nathanhammond/ember-a11y/issues/new?title=${titleStr}`;
+	linkEl.appendChild(document.createTextNode('here'));
+	pEl.appendChild(document.createTextNode('Did this test fail? Let us know '));
+	pEl.appendChild(linkEl);
+	pEl.appendChild(document.createTextNode('.'));
+	return pEl;
+}
+
+export default Ember.Helper.helper(githubIssue);
