6e72aebe278de0009b6acf537992aacb52068759
Ben Holmes <mail@benholmes.uk>
2016-06-05 14:33:55 -0700

    Enable some of the new a11y rules
    Will enable invalid-interactive rule upon fix of https://github.com/rwjblue/ember-template-lint/issues/80

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'img-alt-attributes': true,
+    'invalid-interactive': false, // false until fix of https://github.com/rwjblue/ember-template-lint/issues/80
+    'nested-interactive': true
+  }
 };
